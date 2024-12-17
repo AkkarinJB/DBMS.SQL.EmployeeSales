65057bd495443d4216718e2cfc0f953ae85c73d6
Ge No <geno@MacBook-Air-khxng-Ge.local>
2024-12-17 20:58:35 +0700

    first commit

---
+++ mployeeSalesApp/Models/EmployeeSales.cs
@@ -0,0 +1,8 @@
+﻿public class EmployeeSales
+{
+    public int EmployeeID { get; set; }
+    public string FullName { get; set; }
+    public string Title { get; set; }
+    public int TotalOrders { get; set; }
+    public decimal TotalSales { get; set; }
+}
